c6e2f858bacc0dafe09cbe1f786ac6d5d5662f84
Maze <1054811058@qq.com>
2021-11-11 12:54:33 +0800

    提交代码

---
+++ src/components/fir/3.vue
@@ -0,0 +1,52 @@
+<template>
+    <section>
+        <header>
+            <!-- header slot -->
+            <slot name="header">
+                <th>title</th>
+            </slot>
+        </header>
+        <div class="main">
+            <!-- default slot -->
+            <slot> </slot>
+        </div>
+        <footer>
+            <!-- footer slot -->
+            <slot name="footer"> </slot>
+        </footer>
+    </section>
+</template>
+
+<script>
+// 这里是组件的描述
+export default {
+    name: 'fir-A',
+    props: {
+        // name属性,支持 `.sync`
+        name: {
+            type: [String, Number],
+            required: true,
+            validator () {
+            }
+        },
+        // v-model
+        value: [String, Number]
+    },
+    methods: {
+        /**
+         * 这是一个`async`方法
+         * @param {Bool} type 清除类型
+         * @returns 返回值描述
+         */
+        async clear (type) {
+            // 描述onclear事件
+            this.$emit('onclear', type)
+            return true
+        },
+        _private (val) {
+            this.$emit('update', val)
+            this.$emit('update:name', val)
+        }
+    }
+}
+</script>
